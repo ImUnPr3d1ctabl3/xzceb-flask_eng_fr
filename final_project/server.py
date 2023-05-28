83ae7ba53051d0ad0c7c104d75cd169e2ade6cb7
ImUnPr3d1ctabl3 <115087830+ImUnPr3d1ctabl3@users.noreply.github.com>
2023-05-28 15:50:49 +0200

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -1,29 +1,27 @@
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
-from machinetranslation import english_to_french, french_to_english
-
 
 app = Flask("Web Translator")
 
-@app.route("/english_to_french")
-def english_to_french():
-    text_to_translate = request.args.get('text_to_translate')
+@app.route("/englishToFrench")
+def englishToFrench():
+    textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    translation = english_to_french(text_to_translate)
-    return translation
+    eng_fr = translator.english_to_french(textToTranslate)
+    return eng_fr
 
-@app.route("/french_to_english")
-def french_to_english():
-    text_to_translate = request.args.get('text_to_translate')
+@app.route("/frenchToEnglish")
+def frenchToEnglish():
+    textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    translation = french_to_english(text_to_translate)
-    return translation
+    fr_en = translator.french_to_english(textToTranslate)
+    return fr_en
 
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
-    render_template()
-    
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
