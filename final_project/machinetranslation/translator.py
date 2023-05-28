20303f5ba0d56c2b373210d9068bdd42910f1d0f
ImUnPr3d1ctabl3 <115087830+ImUnPr3d1ctabl3@users.noreply.github.com>
2023-05-28 15:51:27 +0200

    Update translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -4,32 +4,38 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
 
 load_dotenv()
-
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-authenticator = IAMAuthenticator('{apikey}')
+authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
-    version = '2023-01-13',
-    authenticator = authenticator
+    version='2018-05-01',
+    authenticator=authenticator
 )
-
-language_translator.set_service_url('https://api.eu-de.language-translator.watson.cloud.ibm.com')
+language_translator.set_service_url(url)
 
 def english_to_french(english_text):
-    translation = language_translator.translate(
-        text = english_text,
-        source = 'en',
-        target = 'fr'
+    #write the code here
+    if not english_text:
+        return None
+
+    # Translate the text from English to French
+    french_text = language_translator.translate(
+        text=english_text,
+        model_id='en-fr'
     ).get_result()
-    french_text = translation['translations'][0]['translation']
-    return french_text
+
+    return french_text['translations'][0]['translation']
 
 def french_to_english(french_text):
-    translation = language_translator.translate(
-        text = french_text,
-        source = 'fr',
-        target = 'en'
+    #write the code here
+    if not french_text:
+        return None
+
+    # Translate the text from English to French
+    english_text = language_translator.translate(
+        text=french_text,
+        model_id='fr-en'
     ).get_result()
-    english_text = translation['translations'][0]['translation']
-    return english_text
+
+    return english_text['translations'][0]['translation']
